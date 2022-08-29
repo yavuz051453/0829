362a954bfff908173f7ddf80d87175b324938c4b
github_yavuz.selim.ceylan <yavuz.selim.ceylan@gmail.com>
2022-08-29 14:29:22 -0400

    temperature check

---
+++ src/test/java/class01/gitBasics.java
@@ -0,0 +1,9 @@
+package class01;
+
+public class gitBasics {
+    public static void main(String[] args) {
+        //write down a code that checks the temperature and prints out something
+        System.out.println("today is beautiful day");
+
+    }
+}
