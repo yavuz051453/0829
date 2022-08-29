a8dae53c79ff4e91eaf6101efe4fee3e546522ac
github_yavuz.selim.ceylan <yavuz.selim.ceylan@gmail.com>
2022-08-29 17:33:15 -0400

    three lines of additional code

--- src/test/java/class01/gitBasics.java
+++ src/test/java/class01/gitBasics.java
@@ -4,6 +4,8 @@ public class gitBasics {
     public static void main(String[] args) {
         //write down a code that checks the temperature and prints out something
         System.out.println("today isn't beautiful day");
+        System.out.println("today isn't beautiful day");
+        System.out.println("today isn't beautiful day");
 
     }
 }
